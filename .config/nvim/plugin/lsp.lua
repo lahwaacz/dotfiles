ccae61543d1c2c3a29eeecc3bc7d7f8bd63887f6
Jakub Klinkovský <1289205+lahwaacz@users.noreply.github.com>
2024-07-04 23:22:58 +0200

    add lsp.vim

---
+++ .config/nvim/plugin/lsp.lua
@@ -0,0 +1,10 @@
+require'lspconfig'.bashls.setup{}
+require'lspconfig'.clangd.setup{}
+require'lspconfig'.pylsp.setup{}
+require'lspconfig'.texlab.setup{}
+
+require'lspconfig'.cssls.setup{}
+require'lspconfig'.html.setup{}
+require'lspconfig'.jsonls.setup{}
+require'lspconfig'.yamlls.setup{}
+require'lspconfig'.ansiblels.setup{}
