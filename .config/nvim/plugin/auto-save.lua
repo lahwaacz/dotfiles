937376096c0f1148e2792c3e16b3b00e0e50feda
Jakub Klinkovský <1289205+lahwaacz@users.noreply.github.com>
2025-04-27 13:19:23 +0200

    update neovim config

--- .config/nvim/plugin/auto-save.lua
+++ .config/nvim/plugin/auto-save.lua
@@ -7,7 +7,7 @@ require("auto-save").setup {
     -- activate on startup
     enabled = true,
     -- delay after which a pending save is executed
-    debounce_delay = 5000,
+    debounce_delay = 10000,
 }
 
 local group = vim.api.nvim_create_augroup('autosave', {})
@@ -18,7 +18,7 @@ vim.api.nvim_create_autocmd('User', {
     callback = function(opts)
         if opts.data.saved_buffer ~= nil then
             local filename = vim.api.nvim_buf_get_name(opts.data.saved_buffer)
-            print('AutoSave: saved ' .. filename .. ' at ' .. vim.fn.strftime('%H:%M:%S'))
+            vim.notify('AutoSave: saved ' .. filename .. ' at ' .. vim.fn.strftime('%H:%M:%S'), vim.log.levels.INFO)
         end
     end,
 })
--- .config/nvim/plugin/lsp.lua
+++ .config/nvim/plugin/lsp.lua
@@ -3,35 +3,36 @@ if not vim.g.vscode then
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
+vim.lsp.config("*", {
+    capabilities = capabilities,
+})
+
 require'lspconfig'.bashls.setup{
     autostart = false,
-    capabilities = capabilities,
 }
-require'lspconfig'.pkgbuild_language_server.setup{
-    capabilities = capabilities,
-}
-require'lspconfig'.clangd.setup{
-    capabilities = capabilities,
-}
-require'lspconfig'.texlab.setup{
-    capabilities = capabilities,
-}
-require'lspconfig'.neocmake.setup {
-    capabilities = capabilities,
-}
+require'lspconfig'.pkgbuild_language_server.setup{}
+require'lspconfig'.clangd.setup{}
+require'lspconfig'.texlab.setup{}
+require'lspconfig'.neocmake.setup{}
 
 -- ruff does not have completion support
 require'lspconfig'.ruff.setup{}
 -- pyright does static type checking and completion
-require'lspconfig'.pyright.setup{
-    capabilities = capabilities,
-}
+require'lspconfig'.pyright.setup{}
 
 require'lspconfig'.cssls.setup{}
 require'lspconfig'.html.setup{}
 require'lspconfig'.jsonls.setup{}
 require'lspconfig'.yamlls.setup{}
 require'lspconfig'.ansiblels.setup{}
+require'lspconfig'.typos_lsp.setup{}
+require'lspconfig'.vale_ls.setup{
+    settings = {
+        root_markers = {".vale.ini"},
+    },
+    -- do not start the lsp if none of the root markers is found
+    single_file_support = false,
+}
 
 -- starh bashls for filetype=sh, but not filename=PKGBUILD
 vim.api.nvim_create_autocmd(
@@ -48,5 +49,22 @@ vim.api.nvim_create_autocmd(
         end
     }
 )
+
+-- configure efmls for markdownlint-cli2
+-- based on https://github.com/mattn/efm-langserver?tab=readme-ov-file#configuration-for-neovim-builtin-lsp-with-nvim-lspconfig
+require "lspconfig".efm.setup {
+    settings = {
+        root_markers = {".git/"},
+        languages = {
+            markdown = {
+                {
+                    lintCommand = "markdownlint-cli2 -",
+                    lintStdin = true,
+                    lintFormats = { '%f:%l:%c %m', '%f:%l %m', '%f: %l: %m' },
+                }
+            }
+        }
+    }
+}
 
 end
